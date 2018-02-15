e235b697e5cb5e0c4640060b263859048f5a7f20
Alexey Shulutkov <2990660@gmail.com>
2018-02-15 09:37:18 +0700

    update config.rb

--- vagrant/config.rb
+++ vagrant/config.rb
@@ -3,8 +3,8 @@ $update_channel="stable"
 #$controller_count=1
 #$controller_vm_memory=512
 
-$worker_count=1
-$worker_vm_memory=1024
+$worker_count=3
+$worker_vm_memory=2048
 
 #$etcd_count=1
 #$etcd_vm_memory=512
